20e699e5f9242276ba5bc7295607604585e2d079
Martí Paredes <martiparedes19@gmail.com>
2024-02-27 14:26:53 +0100

    Cleaner appearence

--- components/HeroModel.vue
+++ components/HeroModel.vue
@@ -1,40 +1,43 @@
 <template>
   <div class="w-max-2xl h-full flex justify-center items-center z-0 relative">
-    <Transition
-      name="fade-overlay"
-      enter-active-class="opacity-1 transition-opacity duration-200"
-      leave-active-class="opacity-0 transition-opacity duration-200"
-    >
+    <transition-fade>
       <div
-        v-show="!hasFinishLoading"
+        v-if="!hasFinishLoading"
         class="absolute bg-grey-600 t-0 l-0 w-full h-full z-20 flex justify-center items-center text-black font-mono"
       >
-        <div class="w-200px">
-          Loading... {{ progress }} %
-          <i class="i-ic-twotone-catching-pokemon animate-rotate-in"></i>
-        </div>
+        <div class="w-200px">Loading... {{ progress }} %</div>
       </div>
-    </Transition>
-    <TresCanvas v-bind="gl" preset="realistic">
-      <TresPerspectiveCamera :position="[0, 0, 2]" :args="[45, 1, 0.1, 1000]" />
-
-      <Suspense>
-        <GLTFModel
-          path="/aiguille_dibona.glb"
-          scale="0.075"
-          :rotation-y="Math.PI * 1.05"
+    </transition-fade>
+    <transition-fade :duration="2000">
+      <TresCanvas v-show="hasFinishLoading" v-bind="gl" preset="realistic">
+        <TresPerspectiveCamera
+          :position="[0, 0, 2]"
+          :args="[45, 1, 0.1, 1000]"
         />
-      </Suspense>
 
-      <TresDirectionalLight :position="[3, 2, 0]" :intensity="1" cast-shadow />
-      <TresAmbientLight :position="[2, 5, -2]" :intensity="0.75" />
-    </TresCanvas>
+        <Suspense>
+          <GLTFModel
+            path="/aiguille_dibona.glb"
+            scale="0.075"
+            :rotation-y="Math.PI * 1.05"
+          />
+        </Suspense>
+
+        <TresDirectionalLight
+          :position="[3, 2, 0]"
+          :intensity="1"
+          cast-shadow
+        />
+        <TresAmbientLight :position="[2, 5, -2]" :intensity="0.75" />
+      </TresCanvas>
+    </transition-fade>
   </div>
 </template>
 
 <script setup lang="ts">
 import { BasicShadowMap, SRGBColorSpace } from "three";
 import { useProgress } from "@tresjs/cientos";
+import { TransitionFade } from "@morev/vue-transitions";
 
 const { hasFinishLoading, progress, items } = await useProgress();
 
