f30d187973ea96a5e3a68d6e30acfccafb5914ab
Martí Paredes <martiparedes19@gmail.com>
2024-02-27 14:16:45 +0100

    3D model with Trees with loader

--- components/HeroModel.vue
+++ components/HeroModel.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="w-max-2xl h-full flex justify-center items-center z-0 relative">
+    <Transition
+      name="fade-overlay"
+      enter-active-class="opacity-1 transition-opacity duration-200"
+      leave-active-class="opacity-0 transition-opacity duration-200"
+    >
+      <div
+        v-show="!hasFinishLoading"
+        class="absolute bg-grey-600 t-0 l-0 w-full h-full z-20 flex justify-center items-center text-black font-mono"
+      >
+        <div class="w-200px">
+          Loading... {{ progress }} %
+          <i class="i-ic-twotone-catching-pokemon animate-rotate-in"></i>
+        </div>
+      </div>
+    </Transition>
     <TresCanvas v-bind="gl" preset="realistic">
       <TresPerspectiveCamera :position="[0, 0, 2]" :args="[45, 1, 0.1, 1000]" />
 
@@ -19,6 +34,9 @@
 
 <script setup lang="ts">
 import { BasicShadowMap, SRGBColorSpace } from "three";
+import { useProgress } from "@tresjs/cientos";
+
+const { hasFinishLoading, progress, items } = await useProgress();
 
 const gl = {
   shadows: true,
